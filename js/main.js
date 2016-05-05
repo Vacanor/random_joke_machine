131ae0ba5147aa9decbe7c12aef4a8881d8a4cd3
Vacanor <gfariasinfo@gmail.com>
2016-05-05 09:54:41 +0200

    Rewritten some of the code. Added init random joke to the html.

--- js/main.js
+++ js/main.js
@@ -1,51 +1,53 @@
-/*
-  We first get our chuck Norris jokes from the api file
-  You can get the joke api on http://api.icndb.com/jokes/
-*/
+// /*
+//   We get our Chuck Norris jokes from the api file
+//   You can get the joke api on http://api.icndb.com/jokes/
+// */
 
-// We define a variable to store our jokes once we parse the json
-var jokes = [];
+// We initialize vars
+  var joke = "";
+  var jokes = [];
 
-// We create a function that will iterate over the keys in the provided json (wich will contain the response text from the request) and then we push every "insert-key-name-here" entry 
-// from each object (the objects are all within the "value" key in the json) into it's correspondant array
-function storeinfo (resText){
-  // For every object in "value"
-    for(value = 0;value<resText.value.length;value++){
-      // We push the joke of the object into the jokes array
-      jokes.push(resText.value[value].joke);
-    }
+// We create a function that will send the call for the api every time we call it and, when the request is sent sucessfully will pass the data to a function that will use the data (storeinfo)
+function getJoke(){
+  var xmlhttp = new XMLHttpRequest();
+  var url = "api.json";
+
+  xmlhttp.onreadystatechange = function() {
+    // If the request is succesful
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    // We get the objects from our response text
+        var objects = JSON.parse(xmlhttp.responseText);
+        // And we pass them to storeinfo()
+        storeinfo(objects);
+        //console.log(objects[3].joke);
+      }
+    };
+    // We send the request
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+};
+
+// This function will recieve the objects from the request and will sotre the jokes from each object in an array, then, it will select a random number between 0 and the length of the jokes array
+// and finally it will put the random joke in the html. 
+function storeinfo(json){
+  for (object = 0; object < json.length; object++){
+    //console.log(json[object].joke);
+    jokes.push(json[object].joke);
     //console.log(jokes);
-};
-
-// We create a new request object
-var xmlhttp = new XMLHttpRequest();
-// We define the api url
-var url = "api.json";
-
-// We check the request function state (wich will get the response text and call our storeinfo() if the request is succesful)
-xmlhttp.onreadystatechange = function() {
-if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-    var objects = JSON.parse(xmlhttp.responseText);
-    storeinfo(objects);
-    }
-};
-
-// We open and send the request
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
+  }
+  var rndNum = (Math.floor(Math.random() * (jokes.length - 0 + 1) + 0)); // This variable generates the random number each time the function is called
+  joke = jokes[rndNum]; // We select our random joke
+  console.log(joke);
+  $(".quote").html(joke); // We put it in the html
+}
 
 
-function getQuote(){
- 
-       var rndQuote = (Math.floor(Math.random() * (jokes.length - 0 + 1) + 0));
-       $(".quote").html(jokes[rndQuote]);
-};
-// We modify our DOM with the aquired data
-$(document).ready(function() {
-  // We simulate a click to the button
-    getQuote();
-    
+$(document).ready(function(){
+  
+  // We get the init quote the quote container is not empty
+  getJoke();
+
+  // Every time we click we'll call the function to put the joke in the html.
+  $("#getQuote").on("click", getJoke);
+  
 });
-$("#getQuote").on("click", function(){
-       getQuote();
-})
